09cef4ba23572945e1a09a31d80ff8e98e206e13
zohaibxJackie <zkhan52607@gmail.com>
2024-08-11 21:57:56 +0500

    editor width fix

--- src/App.jsx
+++ src/App.jsx
@@ -48,13 +48,13 @@ function App() {
     `)
   }, 250);
 
-  document.addEventListener('keydown', function(event) {
+  document.addEventListener('keydown', function (event) {
     if (event.ctrlKey && (event.key === 'S' || event.key === 's')) {
       event.preventDefault(); // Prevent the default browser action
       setSave(!save);
     }
   });
-  
+
   // whenver there is some change in html, css, js, we will indicate to save the document
   useEffect(() => {
     setIndicateSave(true);
@@ -96,64 +96,65 @@ function App() {
     <>
       <Header setHtml={setHtml} changeLayout={changeLayout} />
       {/* This is the code editors section */}
-      
+
       <div className={`flex ${layoutVarOne ? 'flex-col' : 'flex-row'} overflow-hidden`}>
         {/* The resizable will allow it to resize if the user want to resize */}
-      <Resizable
-      enable={{
-        right: true,   // Allow resizing from the right side only
-        left: false,
-        top: false,
-        bottom: false,
-        topRight: false,
-        bottomRight: false,
-        bottomLeft: false,
-        topLeft: false,
-      }}
-      minWidth={`${layoutVarThird ? '100vw' : '30vw'}`}
-      >
-        <div className={`flex gap-4 pl-3 pr-7 pt-1 min-w-[30vw] bg-[#252526] ${layoutVarOne ? "flex-row" : "flex-col"}`}>
-          <div className={`${htmlGrow} flex flex-col`}>
-            {/* HTml editor */}
-            <div className='flex justify-between bg-black text-white pr-2 pl-4'>
-              <p>{indicateSave ? <GoDotFill className="inline" /> : ''} HTML</p>
-              <button onClick={() => handleLength('HTML')}>{iconToggleHtml ? <BsArrowsAngleContract /> : <BsArrowsAngleExpand />}</button>
+        <Resizable
+          enable={{
+            right: true,   // Allow resizing from the right side only
+            left: false,
+            top: false,
+            bottom: false,
+            topRight: false,
+            bottomRight: false,
+            bottomLeft: false,
+            topLeft: false,
+          }}
+          minWidth={`${layoutVarThird ? '100%' : '30%'}`}
+          maxWidth={'80%'}
+        >
+          <div className={`flex gap-4 px-3 pt-1 min-w-[30vw] bg-[#252526] ${layoutVarOne ? "flex-row" : "flex-col"}`}>
+            <div className={`${htmlGrow} flex flex-col`}>
+              {/* HTml editor */}
+              <div className='flex justify-between bg-black text-white pr-2 pl-4'>
+                <p>{indicateSave ? <GoDotFill className="inline" /> : ''} HTML</p>
+                <button onClick={() => handleLength('HTML')}>{iconToggleHtml ? <BsArrowsAngleContract /> : <BsArrowsAngleExpand />}</button>
+              </div>
+              <Editor
+                language={"xml"}
+                value={html}
+                onchange={setHtml}
+                iconToggleHtml={iconToggleHtml}
+              />
             </div>
-            <Editor
-              language={"xml"}
-              value={html}
-              onchange={setHtml}
-              iconToggleHtml={iconToggleHtml}
-            />
-          </div>
-          <div className={`${cssGrow} flex flex-col`}>
-            {/* CSS editor */}
-            <div className='flex justify-between bg-black text-white pr-2 pl-4'>
-              <p>{indicateSave ? <GoDotFill className="inline" /> : ''} CSS</p>
-              <button onClick={() => handleLength('CSS')}>{iconToggleCss ? <BsArrowsAngleContract /> : <BsArrowsAngleExpand />}</button>
+            <div className={`${cssGrow} flex flex-col`}>
+              {/* CSS editor */}
+              <div className='flex justify-between bg-black text-white pr-2 pl-4'>
+                <p>{indicateSave ? <GoDotFill className="inline" /> : ''} CSS</p>
+                <button onClick={() => handleLength('CSS')}>{iconToggleCss ? <BsArrowsAngleContract /> : <BsArrowsAngleExpand />}</button>
+              </div>
+              <Editor
+                language={"css"}
+                value={css}
+                onchange={setCss}
+                iconToggleCss={iconToggleCss}
+              />
             </div>
-            <Editor
-              language={"css"}
-              value={css}
-              onchange={setCss}
-              iconToggleCss={iconToggleCss}
-            />
-          </div>
-          <div className={`${jsGrow} flex flex-col`}>
-            {/* JS editor */}
-            <div className='flex justify-between bg-black text-white pr-2 pl-4'>
-              <p>{indicateSave ? <GoDotFill className="inline" /> : ''} JS</p>
-              <button onClick={() => handleLength('JS')}>{iconToggleJs ? <BsArrowsAngleContract /> : <BsArrowsAngleExpand />}</button>
+            <div className={`${jsGrow} flex flex-col`}>
+              {/* JS editor */}
+              <div className='flex justify-between bg-black text-white pr-2 pl-4'>
+                <p>{indicateSave ? <GoDotFill className="inline" /> : ''} JS</p>
+                <button onClick={() => handleLength('JS')}>{iconToggleJs ? <BsArrowsAngleContract /> : <BsArrowsAngleExpand />}</button>
+              </div>
+              <Editor
+                language={"javascript"}
+                value={js}
+                onchange={setJs}
+                iconToggleJs={iconToggleJs}
+              />
             </div>
-            <Editor
-              language={"javascript"}
-              value={js}
-              onchange={setJs}
-              iconToggleJs={iconToggleJs}
-            />
           </div>
-        </div>
-      </Resizable>
+        </Resizable>
         {/* this is the ifram section where website is rendered */}
         <div className={`${layoutVarTwo ? 'h-[50vh]' : 'auto'} w-full max-w-full`}>
           <iframe
@@ -161,7 +162,11 @@ function App() {
             height={'100%'}
             width={'100%'}
             title="Output"
-            sandbox="allow-scripts allow-modals"
+            allow="accelerometer *; bluetooth *; camera *; encrypted-media *; display-capture *; geolocation *; gyroscope *; microphone *; midi *; clipboard-read *; clipboard-write *; web-share *; serial *; xr-spatial-tracking *"
+            allowfullscreen="true"
+            allowpaymentrequest="true" allowtransparency="true"
+            sandbox="allow-downloads allow-forms allow-modals allow-pointer-lock allow-popups-to-escape-sandbox allow-popups allow-presentation allow-same-origin allow-scripts allow-top-navigation-by-user-activation"
+            loading="lazy"
           />
         </div>
       </div>
--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -3,28 +3,28 @@ import 'codemirror/theme/material.css'
 import 'codemirror/mode/xml/xml'
 import 'codemirror/mode/css/css'
 import 'codemirror/mode/javascript/javascript'
-import {Controlled as CodeMirror } from 'react-codemirror2'
+import { Controlled as CodeMirror } from 'react-codemirror2'
 
 const Editor = (props) => {
-    const { language, value, onchange, currentLanguage } = props;
-    const handleChange = (editor, data, value) => {
-        onchange(value);
-    }
+  const { language, value, onchange, currentLanguage } = props;
+  const handleChange = (editor, data, value) => {
+    onchange(value);
+  }
 
   return (
-    <>   
-      <CodeMirror
-        onBeforeChange={handleChange}
-        value={value}
-        options={{
-          lineWrapping: true,
-          lint: true,
-          mode: language,
-          theme: "material",
-          lineNumbers: true,
-        }}
-        className='max-w-full'
-      />
+    <>
+        <CodeMirror
+          onBeforeChange={handleChange}
+          value={value}
+          options={{
+            lineWrapping: true,
+            lint: true,
+            mode: language,
+            theme: "material",
+            lineNumbers: true,
+          }}
+          className='max-w-full'
+        />
     </>
   )
 }
